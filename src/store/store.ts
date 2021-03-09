d2b90d32205cd8f2d307058c84d345c45d18b754
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-09 02:21:12 -0300

    feat: add store with useful typed dispatch and appselector

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import productsSlice from 'slices/products-slice';
+
+export const store = configureStore({
+  reducer: { products: productsSlice },
+});
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
