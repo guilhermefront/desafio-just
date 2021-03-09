3457fffcac24f183df8e9eea3a55c41196ae2a03
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-09 12:49:23 -0300

    feat: implement details dynamic props and add button

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -1,6 +1,7 @@
-import React, { Fragment, useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useParams } from 'react-router';
-import { fetchProducts } from 'slices/products-slice';
+import { Link } from 'react-router-dom';
+import { fetchProducts, setCartProducts } from 'slices/products-slice';
 import { useAppDispatch, useAppSelector } from 'store';
 import './details.scss';
 
@@ -8,67 +9,68 @@ const Details = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
 
-  const { productsList } = useAppSelector((state) => state.products);
+  const { productsList, fetchProductsState } = useAppSelector(
+    (state) => state.products
+  );
+
+  const currentProduct = useMemo(
+    () => productsList.find((product) => product.id === Number(id)),
+    [id, productsList]
+  );
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [id, dispatch]);
 
-  return (
+  const handleClick = () => {
+    dispatch(setCartProducts({ edit: 'add', id: Number(id) }));
+  };
+
+  return currentProduct ? (
     <article className="details">
-      {productsList
-        .filter((product) => product.id === Number(id))
-        .map(
-          ({
-            title,
-            description,
-            memory,
-            brand,
-            chipType,
-            price,
-            picture,
-            id,
-          }) => (
-            <Fragment key={id}>
-              <div className="details__info">
-                <h1 className="details__title">{title}</h1>
-                <p className="details__description">{description}</p>
-                <ul className="details__list">
-                  <li className="details__item">
-                    <span className="details__field">Memória</span>
-                    <span className="details__value">{memory}</span>
-                  </li>
-                  <li className="details__item">
-                    <span className="details__field">Marca</span>
-                    <span className="details__value">{brand}</span>
-                  </li>
-                  <li className="details__item">
-                    <span className="details__field">Tipo de chip</span>
-                    <span className="details__value">{chipType}</span>
-                  </li>
-                </ul>
-                <div className="details__buy">
-                  <span className="details__sell">
-                    Á venda por <strong>R${price}</strong>
-                  </span>
-                  <button className="details__cart-button">Comprar</button>
-                </div>
-              </div>
-              <div className="details__img-container">
-                <img
-                  src={picture}
-                  onError={(e) =>
-                    (e.currentTarget.src =
-                      'https://i2.wp.com/cenfewc.com.br/wp-content/uploads/2018/03/imagem-nao-disponivel.jpg?fit=600%2C600&ssl=1')
-                  }
-                  className="details__img"
-                  alt=""
-                />
-              </div>
-            </Fragment>
-          )
-        )}
+      <div className="details__info">
+        <h1 className="details__title">{currentProduct.title}</h1>
+        <p className="details__description">{currentProduct.description}</p>
+        <ul className="details__list">
+          <li className="details__item">
+            <span className="details__field">Memória</span>
+            <span className="details__value">{currentProduct.memory}</span>
+          </li>
+          <li className="details__item">
+            <span className="details__field">Marca</span>
+            <span className="details__value">{currentProduct.brand}</span>
+          </li>
+          <li className="details__item">
+            <span className="details__field">Tipo de chip</span>
+            <span className="details__value">{currentProduct.chipType}</span>
+          </li>
+        </ul>
+        <div className="details__buy">
+          <span className="details__sell">
+            Á venda por <strong>R${currentProduct.price}</strong>
+          </span>
+          <Link to="/cart">
+            <button className="details__cart-button" onClick={handleClick}>
+              Comprar
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="details__img-container">
+        <img
+          src={currentProduct.picture}
+          onError={(e) =>
+            (e.currentTarget.src =
+              'https://i2.wp.com/cenfewc.com.br/wp-content/uploads/2018/03/imagem-nao-disponivel.jpg?fit=600%2C600&ssl=1')
+          }
+          className="details__img"
+          alt=""
+        />
+      </div>
     </article>
-  );
+  ) : fetchProductsState === 'fulfilled' ? (
+    <span className="details__notfind">Produto não encontrado</span>
+  ) : null;
 };
+
 export default Details;
