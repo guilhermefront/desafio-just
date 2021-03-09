9082b79f971e8b83d9f2d46be61b50e2bc78a51e
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-09 12:46:36 -0300

    feat: add all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { Cart, Details, Header, Market } from 'components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'app.scss';
 
-const App = () => <div className="App"></div>;
+const App = () => (
+  <div className="App">
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/cart" component={Cart} />
+        <Route path="/details/:id" component={Details} />
+        <Route component={Market} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
