4be3103b3f78efee638a601294a266fe2ee53a1e
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-09 12:49:48 -0300

    feat: make header total value and price dynamic

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,23 @@
 import cartIcon from 'assets/cart-icon.svg';
 import picture from 'assets/picture.jpeg';
+import useTotal from 'components/utils/total';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from 'store';
 import './header.scss';
 
 const Header = () => {
+  const { cartProductsId } = useAppSelector((state) => state.products);
+
+  const total = useTotal();
+
   return (
     <header className="header">
-      <Link to="/buy" className="header__cart">
-        <img className="header__cart-icon" alt="cart" src={cartIcon} />
+      <span className="header__total">R${total.toFixed(2)}</span>
+      <Link to="/cart">
+        <div className="header__cart">
+          <img className="header__cart-icon" alt="cart" src={cartIcon} />
+          <div className="header__qty">{cartProductsId.length}</div>
+        </div>
       </Link>
       <img className="header__profile" alt="profile" src={picture} />
     </header>
